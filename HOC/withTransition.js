bb24601bf3289058bb2f34a17bde90ecd45eac50
jackzbazuka <milindmoto99@gmail.com>
2022-01-03 17:16:10 +0530

    reformatted withTransition.js

--- HOC/withTransition.js
+++ HOC/withTransition.js
@@ -1,25 +1,23 @@
-import { motion } from "framer-motion";
+import { motion } from 'framer-motion'
 
-const withTransition = (OriginalComponent) => {
-  return () => (
-    <>
-      <OriginalComponent />
-      <motion.div
-        className="slide-in"
-        initial={{ scaleX: 0 }}
-        animate={{ scaleX: 0 }}
-        exit={{ scaleX: 1 }}
-        transition={{ duration: 1, ease: "easeInOut" }}
-      />
-      <motion.div
-        className="slide-out"
-        initial={{ scaleX: 1 }}
-        animate={{ scaleX: 0 }}
-        exit={{ scaleX: 0 }}
-        transition={{ duration: 1, ease: "easeInOut" }}
-      />
-    </>
-  );
-};
-
-export default withTransition;
+export default function withTransition(OriginalComponent) {
+	return () => (
+		<>
+			<OriginalComponent />
+			<motion.div
+				className='slide-in'
+				initial={{ scaleX: 0 }}
+				animate={{ scaleX: 0 }}
+				exit={{ scaleX: 1 }}
+				transition={{ duration: 1, ease: 'easeInOut' }}
+			/>
+			<motion.div
+				className='slide-out'
+				initial={{ scaleX: 1 }}
+				animate={{ scaleX: 0 }}
+				exit={{ scaleX: 0 }}
+				transition={{ duration: 1, ease: 'easeInOut' }}
+			/>
+		</>
+	)
+}
